710488e529c6b9cb20171f6afc112c69b09ad097
GitHub Action <action@github.com>
2024-09-27 06:46:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/gabriel1023/chassishelper/MyBundle.kt
+++ src/main/kotlin/com/github/gabriel1023/chassishelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gabriel1023.chassishelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gabriel1023/chassishelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gabriel1023/chassishelper/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.gabriel1023.chassishelper.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gabriel1023.chassishelper.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
